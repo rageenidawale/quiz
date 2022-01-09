201b8f049dff15b859713aa8c8731f25b764bf9a
Rageeni Dawale <rageenidawale@gmail.com>
2022-01-09 19:38:54 +0530

    add interface and use API to fetch questions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -12,9 +13,10 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
+quiz_interface = QuizInterface(quiz)
 
-while quiz.still_has_question():
-    quiz.next_question()
+# while quiz.still_has_question():
+#     quiz.next_question()
 
 print("You've completed the quiz!")
 print(f"You're final score is {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,5 @@
+import html
+
 class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
@@ -8,17 +10,20 @@ class QuizBrain:
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.check_answer(user_answer, self.current_question.answer)
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            print("You got it right!")
             self.score += 1
+            return True
         else:
-            print("That's wrong.")
-        print(f"The correct answer is: {correct_answer}")
-        print(f"You're current score is: {self.score}/{self.question_number}.")
-        print("\n")
+            return False
+        # print(f"The correct answer is: {correct_answer}")
+        # print(f"You're current score is: {self.score}/{self.question_number}.")
+        # print("\n")
